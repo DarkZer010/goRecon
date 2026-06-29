4f24fff788f35a0b0b2b4f2722ac91536e2eb6b8
DarkZero <ruanvsd2024@gmail.com>
2026-06-29 11:53:31 -0300

    Add files via upload

--- app/app.go
+++ app/app.go
@@ -1,11 +1,10 @@
 package app
 
 import (
-    "github.com/urfave/cli"
-    
     "net"
-    "log"
     "fmt"
+
+    "github.com/urfave/cli"
 )
 
 // Generating will return the command-line application ready to be executed.
@@ -30,44 +29,92 @@ func Gerar() *cli.App {
         	    Action: searchIPs,
         	},
         	{
-        		Name: "servername",
+        		Name: "ns",
         		Usage: "Search for the name of the service.",
         		Flags: flags,
         		Action: searchServersName,
+        	},
+        	{
+        		Name: "cnm",
+        		Usage: "Search for the canonical name record",
+        		Flags: flags,
+        		Action: searchCNAME,
+        	},
+        	{
+        		Name: "txt",
+        		Usage: "Search for TXT records",
+        		Flags: flags,
+        		Action: searchTXT,
         	},
         }
         
         return app
 }
 
-func searchIPs(c *cli.Context) {
+func searchIPs(c *cli.Context) error {
     fmt.Println("----------IPs------------")
 	host := c.String("host")
 
-	ips, erro := net.LookupIP(host)
-	if erro != nil {
-		log.Fatal(erro)
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		return fmt.Errorf("Failed to get the IP address of %s: %w", host, err)
 	}
-
+	
 	for _, ip := range ips {
 		fmt.Println(ip)
 	}
 	
 	fmt.Println("-------------------------\n")
+
+	return nil
 }
 
-func searchServersName(c *cli.Context) {
+func searchServersName(c *cli.Context) error {
     fmt.Println("----------Host NS------------")
     host := c.String("host")
 
-    servers, erro := net.LookupNS(host)
-    if erro != nil {
-    	log.Fatal(erro)
+    servers, err := net.LookupNS(host)
+    if err != nil {
+    	return fmt.Errorf("Failed to get the server name of %s: %w", host, err)
     }
 
     for _, server := range servers {
-    	fmt.Println(server)
+    	fmt.Println(server.Host)
     }
 
     fmt.Println("-------------------------------\n")
+
+    return nil
+}
+
+func searchCNAME(c *cli.Context) error {
+	fmt.Println("-------CNAME-------")
+	host := c.String("host")
+
+	cname, err := net.LookupCNAME(host)
+	if err != nil {
+		return fmt.Errorf("Failed to get the CNAME for %s: %w", host, err)
+	}
+
+	fmt.Println(cname)
+	fmt.Println("-------------------\n")
+
+	return nil
+}
+
+func searchTXT(c *cli.Context) error {
+	fmt.Println("-----------------------------TXT records----------------------------")
+	host := c.String("host")
+
+	txts, erro := net.LookupTXT(host)
+	if erro != nil {
+		return fmt.Errorf("Failed to get TXT records of %s: %w", host, erro)	
+	}
+
+	for _, txt := range txts {
+		fmt.Println(txt)
+	}
+
+	fmt.Println("--------------------------------------------------------------------\n")
+	return nil
 }
